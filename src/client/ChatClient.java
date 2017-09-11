8b8f4191ff025678b6308b8c6a9bc71bd789114a
xdnewlun <xdnewlun@yahoo.com>
2017-09-11 16:03:41 -0500

    added server, working on client

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -1,5 +1,27 @@
 package client;
 
-public class ChatClient {
+import java.io.*;
+import java.net.*;
 
+public class ChatClient {
+	
+	public static void main(String []args) {
+		try {
+			Socket s = new Socket("localhost",6969);
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			while (true) {
+				String message = System.console().readLine();
+				dos.writeUTF(message);
+				System.out.println(dis.readUTF());
+			}
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		Runtime.getRuntime().addShutdownHook(new Thread(public static void main()
+				s.close();
+			}
+		}));	 
+	}
 }
+//https://stackoverflow.com/questions/8051863/how-can-i-close-the-socket-in-a-proper-way
--- src/server/ChatServer.java
+++ src/server/ChatServer.java
@@ -1,5 +1,22 @@
 package server;
 
-public class ChatServer {
+import java.io.*;
+import java.net.*;
 
+public class ChatServer {
+	public static void main(String []args) {
+		try {
+			ServerSocket ss=new ServerSocket(6969);
+			Socket s =ss.accept();
+			DataInputStream dis = new DataInputStream(s.getInputStream());
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+			while (true) {
+				String str=(String)dis.readUTF();
+				dos.writeUTF(str);
+			}
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
 }
